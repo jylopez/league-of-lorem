26c6ba703580e50fde0e1fe9e951ed055c68c692
jylopez <jershonlopez@gmail.com>
2018-05-10 15:55:51 -0600

    styles

--- scripts/Generator.js
+++ scripts/Generator.js
@@ -51,18 +51,21 @@ class Generator extends React.Component {
     })
     return (
       <div className="generator">
-        <a className="accept" onClick={this.rerollHandler}>
-          <img src="/images/reroll.png"/>
-        </a>
-        <div>
-          <select name="paragraph-count" value={this.state.paragraphCount} onChange={this.changeParagraphCount}>
-            <option value="1">1 paragraph</option>
-            <option value="2">2 paragraphs</option>
-            <option value="3">3 paragraphs</option>
-            <option value="4">4 paragraphs</option>
-            <option value="5">5 paragraphs</option>
-          </select>
+        <div className="controls">
+          <a className="accept" onClick={this.rerollHandler}>
+            <img src="/images/reroll.png" />
+          </a>
+          <div className="paragraph-count-wrapper">
+            <select name="paragraph-count" value={this.state.paragraphCount} onChange={this.changeParagraphCount}>
+              <option value="1">1 paragraph</option>
+              <option value="2">2 paragraphs</option>
+              <option value="3">3 paragraphs</option>
+              <option value="4">4 paragraphs</option>
+              <option value="5">5 paragraphs</option>
+            </select>
+          </div>
         </div>
+        
         
         {paragraphs}
       </div>
