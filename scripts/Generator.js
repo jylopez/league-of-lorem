7436fecafa3b415ee5abbdef13689794b5bade6f
jylopez <jershonlopez@gmail.com>
2018-05-10 14:32:58 -0600

    Paragraph count

--- scripts/Generator.js
+++ scripts/Generator.js
@@ -3,25 +3,61 @@
 class Generator extends React.Component {
   constructor(props){
     super(props);
+    this.rerollHandler = this.rerollHandler.bind(this);
     this.generateLorem = this.generateLorem.bind(this);
+    this.changeParagraphCount = this.changeParagraphCount.bind(this);
     this.state = {
-      lorem: LOREM.yasuoLore[0]
+      paragraphs: [LOREM.yasuoLore[0]],
+      paragraphCount: "1"
     }
   }
 
-  generateLorem(){
+  componentWillUpdate(nextProps, nextState){
+    if (nextState.paragraphCount !== this.state.paragraphCount) {
+      this.generateLorem(nextState.paragraphCount)
+    }
+  }
+
+  generateLorem(paragraphCount){
     var items = LOREM.yasuoLore
-    var item = items[Math.floor(Math.random() * items.length)];
+    var paragraphCount = parseInt(paragraphCount)
+    var paragraphs = []
+
+    for (var i = 0; i < paragraphCount; i++) {
+      var randomIndex = Math.floor(Math.random() * items.length);
+      paragraphs.push(items[randomIndex])
+    }
+    
     this.setState({
-      lorem: item
+      paragraphs: paragraphs
+    })
+  }
+
+  rerollHandler(){
+    this.generateLorem(this.state.paragraphCount)
+  }
+
+  changeParagraphCount(e){
+    this.setState({
+      paragraphCount: e.target.value
     })
   }
 
   render(){
+    const paragraphs = this.state.paragraphs.map((paragraph, index) => {
+      return (
+        <p key={index}>{paragraph}</p>
+      )
+    })
     return (
       <div className="generator">
-        <a className="accept" onClick={this.generateLorem}>REROLL!</a>
-        <p>{this.state.lorem}</p>
+        <a className="accept" onClick={this.rerollHandler}>REROLL!</a>
+        <select name="paragraph-count" value={this.state.paragraphCount} onChange={this.changeParagraphCount}>
+          <option value="1">1 paragraph</option>
+          <option value="2">2 paragraphs</option>
+          <option value="3">3 paragraphs</option>
+        </select>
+        {paragraphs}
       </div>
     )
   }
